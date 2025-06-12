60a705580b99b9695143b4b307e9a7155f6eeafc
lmalmud <lmalmud2@jh.edu>
2025-06-12 16:39:54 -0400

    got configuration of environments setup, started notebook for component maps

---
+++ src/zippy/f_a.py
@@ -0,0 +1,104 @@
+'''
+f_a.py
+'''
+
+from zippy.point import Point
+import numpy as np
+from typing import List
+from zippy.utils import sign, f3sqrt
+
+class F_a:
+
+    '''
+    The map f_a as defined in the Geodisc algorithm.
+    '''
+
+    def __init__(self, a: complex):
+        '''
+        Constructor.
+        Parameters:
+        a (complex): the base point of the map
+        '''
+
+        self.a = a.z
+        self.b = np.abs(a) / a.real
+        self.c = np.abs(a) / a.imag
+
+    def f1(self, p: Point) -> Point:
+        '''
+        Applies the Mobius map f_1(z) = \frac{z}{1- \frac{z}{b}}
+        to the given point.
+        Parameters:
+        p (Point): point to apply map to
+        Returns:
+        Point: f_1(p.z)
+        '''
+
+        # f_1(b) = \inf
+        if p.z == 0:
+            return None # FIXME: How do we want to handle this?
+        
+        
+        # FIXME: is it possible that a point very close to the actual base point a
+        # could be interpreted as that a that defines this particular map?
+        # f_1(a) = 0
+        elif p.z == self.a:
+            return Point(z = complex(0, 0),
+                         is_origin = True,
+                         name = p.name,
+                         branch_sign = 0) # It does not matter what branch_sign is
+
+        # Otherwise, map is applied as usual
+        else:
+            z = p.z / (1 - p.z / self.b)
+            return Point(z = z,
+                         is_origin = False,
+                         name = p.name,
+                         branch_sign = sign(z))
+
+    def f2(self, p: Point) -> Point:
+        '''
+        Applies the map f_2(z) = z^2 + c^2 to the given point.
+        Parameters:
+        p (Point): point to apply map to
+        Returns:
+        Point: f_2(p.z)
+        '''
+        z = p.z ** 2 + self.c ** 2
+
+        # Note that we do not update the is_origin flag, as
+        # it is possible for a different point to land at the origin,
+        # but this flag is only used for internal detection of the base point a
+
+        return Point(z = z,
+                     is_origin = p.is_origin,
+                     name = p.name,
+                     branch_sign = p.branch_sign)
+
+    def f3(self, p: Point) -> List[Point]:
+        '''
+        Applies the map f_3(z) = \sqrt{z} with the branch
+        cut along the real axis.
+        Parameters:
+        p (Point): point to apply map to
+        Returns:
+        List[Point]: f_3(z), result of map, potentially multivalued
+        Raises:
+        ValueError if the origin is encountered without is_origin flag
+        '''
+
+        if p.z == 0 and not p.is_origin:
+            raise ValueError("Unexpected: zero encountered in f3 from non-origin point.")
+
+        if p.is_origin:
+            sqrt_val = f3sqrt(p.z)
+            return [
+                Point(z = sqrt_val, is_origin = False, name = p.name, branch_sign = 1),
+                Point(z = sqrt_val, is_origin = False, name = p.name, branch_sign = -1)
+            ]
+        else:
+            sign = p.branch_sign if p.branch_sigh != 0 else 1
+            sqrt_val = sign * f3sqrt(p.z)
+            return [
+                Point(z = sqrt_val, is_origin = False, branch_sign = sign, name = p.name)
+            ]
---
+++ src/zippy/utils.py
@@ -0,0 +1,119 @@
+'''
+utils.py
+Relevant math helper functions.
+'''
+
+import numpy as np
+import colorsys
+from typing import List
+
+def sign(z: complex) -> int:
+    '''
+    Returns 1 if the real part is greater than or equal to
+    zero and -1 otherwise.
+    Note that the imaginary axis is included as "positive."
+    Parameters:
+    z (complex): the input point
+    Returns:
+    int
+    '''
+    if z.real >= 0:
+        return 1
+    else:
+        return -1
+    
+def f3sqrt(z: complex) -> complex:
+    '''
+    Applies the square root function 
+    sqrt(re^{i theta}) = r^{1/2}e^{i theta/2} to z
+    where the branch cut is along (0, \infty) - which
+    means that the argument of the outputs will lie between
+    [0, 2\pi]
+    Parameters:
+    z (complex): point to apply map to
+    Returns:
+    complex: sqrt(z)
+    '''
+
+    # np.angle = tangent function for complex inputs
+    argument = np.angle(z)
+    modulus = np.abs(z)
+
+    # Shift over 2 * np.pi, which defines the branch cut
+    argument = np.mod(argument + 2 * np.pi, 2 * np.pi) - 2 * np.pi
+    
+    return np.sqrt(modulus) * np.exp(1j * argument / 2)
+
+def get_color(z: complex) -> List:
+    '''
+    Returns the color associated with the point at this particular location.
+    Note that this does not update the color.
+
+    https://en.wikipedia.org/wiki/Domain_coloring
+
+    Parameters:
+    z (complex): the point to assign a color to
+    Returns:
+    List: (r, g, b, a)
+    '''
+
+    r = z.real
+    i = z.imag
+    h = np.arctan2(r, i) + (2 * np.pi / 3)
+    h = h / (np.pi * 2)
+    s = 1
+    r = np.sqrt(r ** 2 + i ** 2)
+    l = (2 / np.pi) * np.arctan(r)
+    return colorsys.hls_to_rgb(h, l, s)
+
+def generate_complex(min_r: float, max_r: float, min_c: float, max_c: float, density=10) -> List[complex]:
+    '''
+    Generates a list of points in the complex plane with
+    real value in the range [min_r, max_r] and complex value
+    in the range [min_c, max_c].
+    Parameters:
+    min_r (float), min_c (float): the minimum value for real and complex components, respectively
+    max_r (flaot), max_c (float): the maximum value for real and complex components, respectively
+    density (int): the number of points generated per unit
+    Returns:
+    List[complex]: list of complex values in the given range with specified density
+    '''
+    reals = np.linspace(min_r, max_r, density*(max_r - min_r))
+    imags = np.linspace(min_c, max_c, density*(max_c - min_c))
+    results = []
+    for r in reals:
+        for i in imags:
+            results.append(complex(r, i))
+    return results
+
+def get_coords_from_complex(lst: List[complex]) -> List[float]:
+    '''
+    Given a list of complex points, returns a list of order pairs of
+    real and imaginary parts.
+    Parameters:
+    lst (List[complex]): the list of points to be translated into their components
+    Returns:
+    List[float]
+    '''
+    results = []
+    for item in lst:
+        results.append([item.real, item.complex])
+    return results
+
+def get_real_image_from_complex(lst: List[complex]):
+    '''
+    Given a list of complex points, returns two lists:
+    one with the real components and one with the corresponding
+    imaginary components.
+    Parameters:
+    lst (List[complex]): the list of points to be translated into their components
+    Returns:
+    List[float]: real components
+    List[float]: imaginary components
+    '''
+    reals = []
+    imags = []
+    for p in lst:
+        reals.append(p.real)
+        imags.append(p.imag)
+    return reals, imags
